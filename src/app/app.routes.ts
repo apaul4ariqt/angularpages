417b5b61fb0861cdf58195a28a208ccf85add82d
Arun Paul <apaul@ariqt.com>
2024-05-11 15:50:03 +0100

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', component: NxWelcomeComponent, pathMatch: 'full'},
+    { path: 'products', loadComponent: () => import('@org/modules/products').then((c) => c.ProductsComponent) },
+    { path: 'orders', loadComponent: () => import('@org/modules/orders').then((c) => c.OrdersComponent) },
+];
