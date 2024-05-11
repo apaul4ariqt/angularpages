7e0c603227e714c6494dd18dcdd9e96146335020
Arun Paul <apaul@ariqt.com>
2024-05-11 20:25:46 +0100

    test with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Route } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 export const appRoutes: Route[] = [
-    { path: '', component: NxWelcomeComponent, pathMatch: 'full'},
-    { path: 'products', loadComponent: () => import('@org/modules/products').then((c) => c.ProductsComponent) },
-    { path: 'orders', loadComponent: () => import('@org/modules/orders').then((c) => c.OrdersComponent) },
+    { path: 'angularpages', component: NxWelcomeComponent, pathMatch: 'full'},
+    { path: 'angularpages/products', loadComponent: () => import('@org/modules/products').then((c) => c.ProductsComponent) },
+    { path: 'angularpages/orders', loadComponent: () => import('@org/modules/orders').then((c) => c.OrdersComponent) },
 ];
